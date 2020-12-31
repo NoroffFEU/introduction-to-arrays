4c2f205a6d6e58307ac6cf8b353e24183898d0bb
Connor O'Brien <connor@hifranc.com>
2020-12-31 17:06:08 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+var shoppingList = ["bananas", "a few frogs", "some rats"];
+
+console.log(shoppingList);
+
+// number of items in array 
+console.log("length: ", shoppingList.length);
+
+// add item to array - push()
+shoppingList.push("rat poison");
+
+console.log(shoppingList);
+
+console.log("length: ", shoppingList.length);
+
+// sort array - sort()
+// console.log(shoppingList.sort());
+
+var sortedList = shoppingList.sort();
+console.log(sortedList);
